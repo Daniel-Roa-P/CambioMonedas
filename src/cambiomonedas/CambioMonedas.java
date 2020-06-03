180f557f582406acfdd24d84661119f8fd77feaf
DanielRoa <danbron523@gmail.com>
2020-06-03 08:57:50 -0500

    calcula el mejor cambio pero sin mostrar la ruta

--- src/cambiomonedas/CambioMonedas.java
+++ src/cambiomonedas/CambioMonedas.java
@@ -3,14 +3,15 @@ package cambiomonedas;
 
 import java.awt.Color;
 import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javafx.scene.layout.Border;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.WindowConstants;
@@ -35,22 +36,20 @@ public class CambioMonedas extends JFrame implements ActionListener{
     JButton calcular = new JButton("Calcular");
 
     JPanel jpan1 = new JPanel(); 
-    JPanel jpan2 = new JPanel();
     JPanel jpan3 = new JPanel();
     
-    int valorOptimo;
-    
-    int columA,columB,filasF,columF;
-
-    int contador;
+    JScrollPane scrollPane = new JScrollPane();
+    JScrollPane scrollPane1 = new JScrollPane();
     
     int monedas, vueltas;
+    
+    javax.swing.border.Border border = BorderFactory.createLineBorder(Color.BLACK);
     
     public static void main(String[] args) {
         
         CambioMonedas moneda = new CambioMonedas();
         moneda.setSize(1200, 710);
-        moneda.setTitle("Problema de la mochila");
+        moneda.setTitle("Cambio Monedas");
         moneda.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         moneda.setVisible(true); 
         
@@ -63,8 +62,9 @@ public class CambioMonedas extends JFrame implements ActionListener{
         this.getContentPane().setBackground(Color.LIGHT_GRAY);
         
         c.add(jpan1);
-        c.add(jpan2);
         c.add(jpan3);
+        
+        c.add(scrollPane1);
         
         c.add(texto1);
         c.add(texto2);
@@ -91,14 +91,17 @@ public class CambioMonedas extends JFrame implements ActionListener{
         ingresar.setBounds(330, 20, 100, 20);
         calcular.addActionListener(this);
         calcular.setBounds(330, 50, 100, 20);
-     
+        
         jpan1.setBounds(10, 120, 250, 70);
         jpan1.setBackground(Color.WHITE);
-        jpan2.setBounds(10, 220, 1150, 400);
-        jpan2.setBackground(Color.DARK_GRAY);
         jpan3.setBounds(350, 120, 220, 70);
         jpan3.setBackground(Color.WHITE);
         
+        scrollPane.setBounds(10, 220, 2000, 2000);
+        scrollPane.setPreferredSize(new Dimension(2000, 2000));  
+        
+        scrollPane1.setBounds(10, 220, 1150, 400);
+        scrollPane1.setPreferredSize(new Dimension(1150, 400)); 
         
     }
     
@@ -121,12 +124,15 @@ public class CambioMonedas extends JFrame implements ActionListener{
             
             jpan1.repaint();
             
+            int x=5;
+            
             for(int i = 0; i<monedas;i++){
                  
-                        arregloMonedas[i] = new JTextField(Integer.toString(i));
-                        arregloMonedas[i].setBounds(i*30,30,20,20);
-                        jpan1.add(arregloMonedas[i]);
-                    
+                    arregloMonedas[i] = new JTextField(Integer.toString(x));
+                    arregloMonedas[i].setBounds(i*30,30,20,20);
+                    jpan1.add(arregloMonedas[i]);
+                    x--;
+                        
                 }
             
         } else if(e.getSource() == calcular) {
@@ -147,7 +153,7 @@ public class CambioMonedas extends JFrame implements ActionListener{
             
             }
             
-            jpan2.removeAll();
+            scrollPane.removeAll();
             
             matriz = new JTextArea[monedas + 2][vueltas + 2];
             
@@ -165,7 +171,7 @@ public class CambioMonedas extends JFrame implements ActionListener{
                         
                     } else if(i == 1 && j>0){
                     
-                        matriz[i][j] = new JTextArea(Integer.toString(999));
+                        matriz[i][j] = new JTextArea(Double.toString(Double.POSITIVE_INFINITY));
                         
                     }else if(i > 1 && j == 0){
                     
@@ -173,13 +179,80 @@ public class CambioMonedas extends JFrame implements ActionListener{
                         
                     } else {
                     
-                        if( (i == 2) && (j < Integer.parseInt(arregloMonedas[i-2].getText())) ){
-                            
-                            
-                            
-                        }else{
+                        int valor = 0;
+                        
+                        if(i > 1){
+                        
+                            valor = Integer.parseInt(arregloMonedas[i-2].getText());
+                        
+                        }
+                        
+                        if(j==1 && i>1){
                             
                             matriz[i][j] = new JTextArea(Integer.toString(0));
+                            
+                        } else if( (i == 2) && (j <= valor) ){
+                            
+                            matriz[i][j] = new JTextArea( Double.toString(Double.POSITIVE_INFINITY) );
+                            
+                        } else if( (i == 2) && (j >= valor)){
+                            
+                            String cadena = matriz[i][j - valor ].getText();
+                            
+                            
+                            
+                            double cantidad = 1 + Double.parseDouble(cadena);
+                            
+                            if(cantidad != Double.POSITIVE_INFINITY){
+                                
+                                matriz[i][j] = new JTextArea(Double.toString(cantidad));
+                            
+                                
+                            } else {
+                                
+                                matriz[i][j] = new JTextArea(Double.toString(cantidad));
+                                 
+                            }
+                            
+                        } else if( j < valor){
+                            
+                            String cadena = matriz[i-1][j].getText();
+                            
+                            
+                            double cantidad = Double.parseDouble(cadena);
+                            
+                            if(cantidad != Double.POSITIVE_INFINITY){
+                                
+                                matriz[i][j] = new JTextArea(Double.toString(cantidad));
+                            
+                                
+                            } else {
+                                
+                                matriz[i][j] = new JTextArea(Double.toString(cantidad));
+                                 
+                            }
+                            
+                            
+                        }else if(i>2){
+                        
+                            
+                            double a = Double.parseDouble(matriz[i-1][j].getText());
+                            double b = 1 + Double.parseDouble(matriz[i][j - valor].getText());
+                            
+                            if(a<=b){
+
+                                matriz[i][j] = new JTextArea(Double.toString(a));
+                                
+                            } else {
+
+                                matriz[i][j] = new JTextArea(Double.toString(b));
+
+                            }
+//                            
+
+                        }else {           
+                           
+                            matriz[i][j] = new JTextArea("0");
                             
                         }
                         
@@ -188,21 +261,22 @@ public class CambioMonedas extends JFrame implements ActionListener{
                         
                     }
                     
-                    
-                    matriz[i][j].setBounds(j*60 + 5 ,i*70 + 5,50,60);
-                    jpan2.add(matriz[i][j]);
+                    matriz[i][j].setBorder(border);
+                    matriz[i][j].setBounds(j*60 + 5 ,i*110 + 5,50,90);
+                    scrollPane.add(matriz[i][j]);
                     
                 }
                 
             }
             
-            jpan2.repaint();
+            scrollPane.repaint();
+            scrollPane1.setViewportView(scrollPane);
             
         }
         
     }
     
-    int hallarMin( int a, int b){
+    double hallarMin( double a, double b){
         
         if(a<b){
             
